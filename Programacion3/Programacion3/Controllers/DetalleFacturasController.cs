d615f2949043f3241189633c753ec922c6869e16
polanco15 <araidepolanco@gmail,com>
2020-04-07 23:32:37 -0400

    Controlador Detalle de factura

---
+++ Programacion3/Programacion3/Controllers/DetalleFacturasController.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Programacion3.Models;
+
+namespace Programacion3.Controllers
+{
+    public class DetalleFacturasController : Controller
+    {
+        private FinalContext db = new FinalContext();
+
+        // GET: DetalleFacturas
+        public ActionResult Index()
+        {
+            var detalleFacturas = db.DetalleFacturas.Include(d => d.Factura).Include(d => d.Producto);
+            return View(detalleFacturas.ToList());
+        }
+
+        // GET: DetalleFacturas/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            DetalleFactura detalleFactura = db.DetalleFacturas.Find(id);
+            if (detalleFactura == null)
+            {
+                return HttpNotFound();
+            }
+            return View(detalleFactura);
+        }
+
+        // GET: DetalleFacturas/Create
+        public ActionResult Create()
+        {
+            ViewBag.IdFactura = new SelectList(db.Facturas, "IdFactura", "IdFactura");
+            ViewBag.IdProducto = new SelectList(db.Productos, "IdProducto", "Nombre");
+            return View();
+        }
+
+        // POST: DetalleFacturas/Create
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que desea enlazarse. Para obtener 
+        // más información vea https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "IdDetalle,IdFactura,IdProducto,Cantidad,Precio,SubTotal")] DetalleFactura detalleFactura)
+        {
+            if (ModelState.IsValid)
+            {
+                db.DetalleFacturas.Add(detalleFactura);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.IdFactura = new SelectList(db.Facturas, "IdFactura", "IdFactura", detalleFactura.IdFactura);
+            ViewBag.IdProducto = new SelectList(db.Productos, "IdProducto", "Nombre", detalleFactura.IdProducto);
+            return View(detalleFactura);
+        }
+
+        // GET: DetalleFacturas/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            DetalleFactura detalleFactura = db.DetalleFacturas.Find(id);
+            if (detalleFactura == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.IdFactura = new SelectList(db.Facturas, "IdFactura", "IdFactura", detalleFactura.IdFactura);
+            ViewBag.IdProducto = new SelectList(db.Productos, "IdProducto", "Nombre", detalleFactura.IdProducto);
+            return View(detalleFactura);
+        }
+
+        // POST: DetalleFacturas/Edit/5
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que desea enlazarse. Para obtener 
+        // más información vea https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "IdDetalle,IdFactura,IdProducto,Cantidad,Precio,SubTotal")] DetalleFactura detalleFactura)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(detalleFactura).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.IdFactura = new SelectList(db.Facturas, "IdFactura", "IdFactura", detalleFactura.IdFactura);
+            ViewBag.IdProducto = new SelectList(db.Productos, "IdProducto", "Nombre", detalleFactura.IdProducto);
+            return View(detalleFactura);
+        }
+
+        // GET: DetalleFacturas/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            DetalleFactura detalleFactura = db.DetalleFacturas.Find(id);
+            if (detalleFactura == null)
+            {
+                return HttpNotFound();
+            }
+            return View(detalleFactura);
+        }
+
+        // POST: DetalleFacturas/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            DetalleFactura detalleFactura = db.DetalleFacturas.Find(id);
+            db.DetalleFacturas.Remove(detalleFactura);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
