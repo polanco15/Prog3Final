81c7d8704d317a23d3aff7828a4ef3c7d81d2d63
polanco15 <araidepolanco@gmail,com>
2020-04-07 22:58:42 -0400

    Creacion de tablas
    Creacion de tablas con Dbset

--- Programacion3/Programacion3/Models/FinalContext.cs
+++ Programacion3/Programacion3/Models/FinalContext.cs
@@ -2,11 +2,22 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
-
+using System.Data.Entity;
 namespace Programacion3.Models
 {
-    public class FinalContext
+    public class FinalContext : DbContext
     {
+        public FinalContext()
+            : base("Cadena1")
+        { }
+
+        public DbSet<Producto> Productos { get; set; }
+        public DbSet<Cliente> Clientes { get; set; }
+        public DbSet<Categorias> Categorias { get; set; }
+        public DbSet<DetalleFactura> DetalleFacturas { get; set; }
+        public DbSet<EntradaMercancia> EntradaMercancias { get; set; }
+        public DbSet<Proveedor> Proveedors { get; set; }
+        public DbSet<Factura> Facturas { get; set; }
 
     }
 }
