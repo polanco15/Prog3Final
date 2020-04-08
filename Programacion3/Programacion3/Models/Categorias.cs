bd0aeb479d4cd066f388c1fcb015c0d600b81770
polanco15 <araidepolanco@gmail,com>
2020-04-07 22:30:05 -0400

    Factura

--- Programacion3/Programacion3/Models/Categorias.cs
+++ Programacion3/Programacion3/Models/Categorias.cs
@@ -11,8 +11,21 @@ namespace Programacion3.Models
     {
         [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         [Key]
-        public int IDCategoria { get; set; }
+        public int IdFactura { get; set; }
+
+        public int IdCliente { get; set; }
+
+        [ForeignKey("IdCliente")]
+        public Cliente Cliente { get; set; }
+
+        public decimal SubTotal { get; set; }
+        public decimal Descuento { get; set; }
+        public decimal ITBIS { get; set; }
+        public decimal Total { get; set; }
         [Required]
-        public string Categoria { get; set; }
+        public DateTime FechaFactura { get; set; }
+        [Required]
+        public DateTime FechaCreacion { get; set; }
+
     }
 }
