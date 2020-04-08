1d29f03de7c606732152d54d927aec1020b25d51
polanco15 <araidepolanco@gmail,com>
2020-04-08 08:52:01 -0400

    Css

--- Programacion3/Programacion3/Controllers/CategoriasController.cs
+++ Programacion3/Programacion3/Controllers/CategoriasController.cs
@@ -42,92 +42,92 @@ namespace Programacion3.Controllers
         }
 
         // GET: Categorias/Create
-        public ActionResult Create()
-        {
-            return View();
-        }
+        //public ActionResult Create()
+        //{
+        //    return View();
+        //}
 
-        // POST: Categorias/Create
-        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que desea enlazarse. Para obtener 
-        // más información vea https://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Create([Bind(Include = "IdCategoria,Categoria")] Categorias categorias)
-        {
-            if (ModelState.IsValid)
-            {
-                db.Categorias.Add(categorias);
-                db.SaveChanges();
-                return RedirectToAction("Index");
-            }
+        //// POST: Categorias/Create
+        //// Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que desea enlazarse. Para obtener 
+        //// más información vea https://go.microsoft.com/fwlink/?LinkId=317598.
+        //[HttpPost]
+        //[ValidateAntiForgeryToken]
+        //public ActionResult Create([Bind(Include = "IdCategoria,Categoria")] Categorias categorias)
+        //{
+        //    if (ModelState.IsValid)
+        //    {
+        //        db.Categorias.Add(categorias);
+        //        db.SaveChanges();
+        //        return RedirectToAction("Index");
+        //    }
 
-            return View(categorias);
-        }
+        //    return View(categorias);
+        //}
 
-        // GET: Categorias/Edit/5
-        public ActionResult Edit(int? id)
-        {
-            if (id == null)
-            {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-            }
-            Categorias categorias = db.Categorias.Find(id);
-            if (categorias == null)
-            {
-                return HttpNotFound();
-            }
-            return View(categorias);
-        }
+        //// GET: Categorias/Edit/5
+        //public ActionResult Edit(int? id)
+        //{
+        //    if (id == null)
+        //    {
+        //        return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+        //    }
+        //    Categorias categorias = db.Categorias.Find(id);
+        //    if (categorias == null)
+        //    {
+        //        return HttpNotFound();
+        //    }
+        //    return View(categorias);
+        //}
 
-        // POST: Categorias/Edit/5
-        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que desea enlazarse. Para obtener 
-        // más información vea https://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Edit([Bind(Include = "IdCategoria,Categoria")] Categorias categorias)
-        {
-            if (ModelState.IsValid)
-            {
-                db.Entry(categorias).State = EntityState.Modified;
-                db.SaveChanges();
-                return RedirectToAction("Index");
-            }
-            return View(categorias);
-        }
+        //// POST: Categorias/Edit/5
+        //// Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que desea enlazarse. Para obtener 
+        //// más información vea https://go.microsoft.com/fwlink/?LinkId=317598.
+        //[HttpPost]
+        //[ValidateAntiForgeryToken]
+        //public ActionResult Edit([Bind(Include = "IdCategoria,Categoria")] Categorias categorias)
+        //{
+        //    if (ModelState.IsValid)
+        //    {
+        //        db.Entry(categorias).State = EntityState.Modified;
+        //        db.SaveChanges();
+        //        return RedirectToAction("Index");
+        //    }
+        //    return View(categorias);
+        //}
 
-        // GET: Categorias/Delete/5
-        public ActionResult Delete(int? id)
-        {
-            if (id == null)
-            {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-            }
-            Categorias categorias = db.Categorias.Find(id);
-            if (categorias == null)
-            {
-                return HttpNotFound();
-            }
-            return View(categorias);
-        }
+        //// GET: Categorias/Delete/5
+        //public ActionResult Delete(int? id)
+        //{
+        //    if (id == null)
+        //    {
+        //        return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+        //    }
+        //    Categorias categorias = db.Categorias.Find(id);
+        //    if (categorias == null)
+        //    {
+        //        return HttpNotFound();
+        //    }
+        //    return View(categorias);
+        //}
 
-        // POST: Categorias/Delete/5
-        [HttpPost, ActionName("Delete")]
-        [ValidateAntiForgeryToken]
-        public ActionResult DeleteConfirmed(int id)
-        {
-            Categorias categorias = db.Categorias.Find(id);
-            db.Categorias.Remove(categorias);
-            db.SaveChanges();
-            return RedirectToAction("Index");
-        }
+        //// POST: Categorias/Delete/5
+        //[HttpPost, ActionName("Delete")]
+        //[ValidateAntiForgeryToken]
+        //public ActionResult DeleteConfirmed(int id)
+        //{
+        //    Categorias categorias = db.Categorias.Find(id);
+        //    db.Categorias.Remove(categorias);
+        //    db.SaveChanges();
+        //    return RedirectToAction("Index");
+        //}
 
-        protected override void Dispose(bool disposing)
-        {
-            if (disposing)
-            {
-                db.Dispose();
-            }
-            base.Dispose(disposing);
-        }
+        //protected override void Dispose(bool disposing)
+        //{
+        //    if (disposing)
+        //    {
+        //        db.Dispose();
+        //    }
+        //    base.Dispose(disposing);
+        //}
     }
 }
