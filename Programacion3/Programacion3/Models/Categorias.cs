dc31ba99d96a39a740fd49f71614fb94c0b8e688
polanco15 <araidepolanco@gmail,com>
2020-04-07 22:38:02 -0400

    Detalle de factura
    Aqui se hace se hace la tabla de detalle del la factura , la descripcion

--- Programacion3/Programacion3/Models/Categorias.cs
+++ Programacion3/Programacion3/Models/Categorias.cs
@@ -11,21 +11,11 @@ namespace Programacion3.Models
     {
         [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         [Key]
-        public int IdFactura { get; set; }
-
-        public int IdCliente { get; set; }
-
-        [ForeignKey("IdCliente")]
-        public Cliente Cliente { get; set; }
-
-        public decimal SubTotal { get; set; }
-        public decimal Descuento { get; set; }
-        public decimal ITBIS { get; set; }
-        public decimal Total { get; set; }
+        public int IDCategoria { get; set; }
         [Required]
-        public DateTime FechaFactura { get; set; }
-        [Required]
-        public DateTime FechaCreacion { get; set; }
+        public string Categoria { get; set; }
+
+        
 
     }
 }
